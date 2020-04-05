caa9cd8b5f36664c10050f9426646ae6a4a96b53
Gutei <anggeraryp@gmail.com>
2020-04-05 22:00:44 +0700

    add tag templates

--- dash/dramasia/views/home.py
+++ dash/dramasia/views/home.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 def home(request):
     context = {}
-    return render(request, 'dramasia/index.html', context)
+    return render(request, 'dramasia/home.html', context)
