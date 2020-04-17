a6bd32cb1d452452e75fefad8b67b6ba35baf520
liokta <shiroasasin@gmail.com>
2020-04-17 10:52:34 +0700

    Fix season

--- dash/dramasia/views/home.py
+++ dash/dramasia/views/home.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-from dramasia.models import Drama, Season
+from dramasia.models import Drama, Season, DramaSeason
 
 def home(request):
-    this_season = Drama.objects.filter(is_publish=True).order_by('-updated')[:8]
+    # this_season = Drama.objects.filter(is_publish=True).order_by('-updated')[:8]
+    season = Season.objects.filter(is_season=True).first()
+    drama_season = DramaSeason.objects.filter(season=season).order_by('-updated')[:8]
+    ds_list = []
+    for ds in drama_season:
+        ds_list.append(ds.drama)
+
     drama = Drama.objects.filter(is_publish=True).order_by('-updated')[:6]
     context = {
-        'this_season': this_season,
+        'this_season': ds_list,
         'drama': drama
     }
     return render(request, 'dramasia/home.html', context)
