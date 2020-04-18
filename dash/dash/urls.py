86e7d665b0f3004c205e1d4d7d5e185c99db7aa4
liokta <shiroasasin@gmail.com>
2020-04-18 18:55:34 +0700

    Prod

--- dash/dash/urls.py
+++ dash/dash/urls.py
@@ -66,7 +66,7 @@ urlpatterns = [
                   url(r'^content/(?P<pk>[^/]+)/$', views.get_movie, name='get_movie'),
                   url(r'^cast-list/$', views.listing_cast, name='casts_list'),
                   url(r'^post-review-movie/(?P<pk>[^/]+)/$', views.post_review, name='post_review_movie'),
-                  url(r'^developer/stellarium', permission_required('is_staff')(schema_view)    ),
+                  url(r'^developer/stellarium', permission_required('is_staff')(schema_view)),
                   url(r'^nebula/v1/', include(router.urls)),
                   url(r'^api-auth/', include('rest_framework.urls',  namespace='rest_framework')),
 
