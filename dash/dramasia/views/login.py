a94db2a179dcc6ef89bb8a8d21727cf20576e912
liokta <shiroasasin@gmail.com>
2020-04-19 01:28:32 +0700

    Login user

--- dash/dramasia/views/login.py
+++ dash/dramasia/views/login.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
 
 def login(request):
     context = {}
@@ -8,4 +13,18 @@ def login(request):
 
 
 
-
+def auth(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse("Your account was inactive.")
+        else:
+            return HttpResponse("Invalid login details given")
+    else:
+        return render(request, 'dramasia/login.html', {})
