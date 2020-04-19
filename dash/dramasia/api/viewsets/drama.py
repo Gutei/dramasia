5cb1062227d2f5bbfdfab1bd64b71f10c65c27b9
liokta <shiroasasin@gmail.com>
2020-04-19 17:02:29 +0700

    Feature(API-fix): get list country

--- dash/dramasia/api/viewsets/drama.py
+++ dash/dramasia/api/viewsets/drama.py
@@ -104,10 +104,10 @@ class DramaViewSet(viewsets.ModelViewSet):
                 return Response({'message': 'Token is not valid.'}, status=status.HTTP_403_FORBIDDEN)
 
         country = Drama.objects.values('country').distinct()
-        if country:
-            data = country.country
-        else:
-            data = []
+        data = []
+
+        for c in country:
+            data.append(c.country)
 
         return Response(data, status=status.HTTP_200_OK)
 
